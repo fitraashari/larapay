5806008fc660a1111e40de65b2c6c36c29c8741c
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-09-15 15:43:39 +0700

    core api midtrans

---
+++ app/Http/Controllers/Api/PaymentController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Order;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Str;
+
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        request()->validate([
+            'amount'=>['required','numeric'],
+            'method'=>['required']
+        ]);
+        $order = Order::create([
+            'amount'=>request('amount'),
+            'method'=>request('method')
+        ]);
+        $order->order_id = $order->id.'-'. Str::random(5);
+        $order->save();
+        
+        $response_midtrans = $this->midtrans_store($order);
+
+        return response()->json([
+            'response_code'=>'00',
+            'response_status'=>'success',
+            'data'=>$response_midtrans
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function midtrans_store(Order $order){
+        $server_key = base64_encode(config('app.midtrans.server_key'));
+        $base_uri = config('app.midtrans.base_uri');
+        // dd($base_uri);
+        $client = new Client([
+            'base_uri'=>$base_uri
+        ]);
+        $headers = [
+            'Accept'=>'application/json',
+            'Authorization'=>'Basic '.$server_key,
+            'Content-Type'=>'application/json',
+        ];
+
+        switch ($order->method){
+            case 'bca':
+                $body=[
+                    'payment_type'=> 'bank_transfer',
+                    'transaction_details'=> [
+                        'order_id'=> $order->order_id,
+                        'gross_amount'=> $order->amount
+                    ],
+                    'bank_transfer'=>[
+                        'bank'=> 'bca'
+                    ]
+                ];
+                break;
+            case 'permata':
+                $body=[
+                    'payment_type'=> 'permata',
+                    'transaction_details'=> [
+                        'order_id'=> $order->order_id,
+                        'gross_amount'=> $order->amount
+                    ]
+                ];
+                break;
+            default:
+                $body=[];
+            break;
+        }
+        $res = $client->post('/v2/charge',[
+            'headers'=>$headers,
+            'body'=>json_encode($body)
+        ]);
+            return json_decode($res->getBody());
+    }
+}
